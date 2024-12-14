2433bdce1ace377af588353c6683e8db9e961337
zzzcb <2369148217@qq.com>
2024-12-14 11:59:26 +0800

    dev

--- src/multilspy/__init__.py
+++ src/multilspy/__init__.py
@@ -4,5 +4,6 @@ This module contains the multilspy API
 
 from . import multilspy_types as Types
 from .language_server import LanguageServer, SyncLanguageServer
+from . import cli
 
-__all__ = ["LanguageServer", "Types", "SyncLanguageServer"]
+__all__ = ["LanguageServer", "Types", "SyncLanguageServer", "cli"]
